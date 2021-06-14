63da02e7f682022da0432c58dd0b64a93366bd47
alvin almodal <almodalalvin@gmail.com>
2021-06-14 18:53:06 +0800

    first iteration

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 var app = express();
 
@@ -9,6 +11,11 @@ app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  let { originalname: name, mimetype: type, size } = req.file;
+  res.send({ name, type, size });
 });
 
 const port = process.env.PORT || 3000;
